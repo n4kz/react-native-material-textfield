acefde27be92756d12b3bc0bd04ff14e02661c14
n4kz <n4kz@n4kz.com>
2017-05-10 11:11:00 +0300

    Removed unused propTypes from Line component

--- src/components/line/index.js
+++ src/components/line/index.js
@@ -4,26 +4,20 @@ import { View } from 'react-native';
 import styles from './styles';
 
 export default class Line extends PureComponent {
-  static defaultProps = {
-    pointerEvents: 'none',
-  };
-
   static propTypes = {
-    ...View.propTypes,
-
     type: PropTypes.oneOf(['solid', 'dotted', 'dashed']).isRequired,
     color: PropTypes.string.isRequired,
   };
 
   render() {
-    let { color: borderColor, type: borderStyle, ...props } = this.props;
+    let { color: borderColor, type: borderStyle } = this.props;
 
     let lineStyle = {
       borderColor, borderStyle,
     };
 
     return (
-      <View style={[styles.line, lineStyle]} {...props} />
+      <View style={[styles.line, lineStyle]} pointerEvents='none' />
     );
   }
 }
