4a963a0902ef0a33336fd2a7046a47c8558bab5f
tctaxien <tc@taxien.com>
2020-04-25 22:33:12 -0400

    Update index.js

--- src/components/label/index.js
+++ src/components/label/index.js
@@ -43,7 +43,7 @@ export default class Label extends PureComponent {
       y1: PropTypes.number,
     }),
 
-    style: Animated.Text.propTypes.style,
+    style: PropTypes.any,
     label: PropTypes.string,
   };
 
