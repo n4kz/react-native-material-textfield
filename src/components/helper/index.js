ce0b1868c8f2c6187e6f32ba4608d4f12669b0ac
ruslandjent <sayfulinruslan.djnt@gmail.com>
2020-06-25 23:45:33 +0500

    linter fixes

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import { View, Animated, Text } from 'react-native';
+import { Animated, Text } from 'react-native';
 
 import styles from './styles';
 
