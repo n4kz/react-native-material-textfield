b05945ff45d524146340c9a51065cf5823609da8
ahuacate15 <carlos.itca@gmail.com>
2020-09-04 19:31:56 -0600

    fix fail check

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import { Animated } from 'react-native';
+import { Animated, Text } from 'react-native';
 
 import styles from './styles';
 
@@ -11,6 +11,7 @@ export default class Helper extends PureComponent {
 
     disabled: PropTypes.bool,
 
+    style: Text.propType,
 
     baseColor: PropTypes.string,
     errorColor: PropTypes.string,
