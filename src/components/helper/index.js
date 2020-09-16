9b691236da3f9f9e06c0175c84931ae736339919
Ersin Kurtanović <ersin.kurtanovic@orka-global.com>
2020-09-16 22:07:59 +0200

    fix warnings

--- src/components/helper/index.js
+++ src/components/helper/index.js
@@ -11,7 +11,7 @@ export default class Helper extends PureComponent {
 
     disabled: PropTypes.bool,
 
-    style: Text.propType,
+    style:  PropTypes.any,
 
     baseColor: PropTypes.string,
     errorColor: PropTypes.string,
--- src/components/label/index.js
+++ src/components/label/index.js
@@ -43,7 +43,7 @@ export default class Label extends PureComponent {
       y1: PropTypes.number,
     }),
 
-    style: Text.propType,
+    style: PropTypes.any,
     label: PropTypes.string,
   };
 
